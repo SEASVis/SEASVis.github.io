cfbdd84f275b3043fd5dacdb60f3adad4e07c5f5
zanewolf <zwolf.mlxvi@gmail.com>
2021-11-10 10:24:46 -0500

    added center text

--- js/vis_radial.js
+++ js/vis_radial.js
@@ -1,6 +1,7 @@
 // Data
 let departments;
 let nodes;
+let hover;
 
 class visRadial {
 
@@ -23,20 +24,34 @@ class visRadial {
         vis.center=[vis.width/2, vis.height/2];
 
         // the radius of the node circles
-        vis.radiusAreas = (Math.min(vis.width, vis.height)  - vis.margin.top)/ 4.5
-        vis.radiusCenters = (Math.min(vis.width, vis.height) - vis.margin.top - vis.margin.bottom) / 2.5
-        vis.radiusFaculty = (Math.min(vis.width, vis.height)  - vis.margin.top - vis.margin.bottom)/ 4
-
+        vis.radiusAreas = (Math.min(vis.width, vis.height)  - vis.margin.top)/ 6
+        vis.radiusCenters = (Math.min(vis.width, vis.height) - vis.margin.top - vis.margin.bottom) / 3
+        vis.radiusFaculty = (Math.min(vis.width, vis.height)  - vis.margin.top - vis.margin.bottom)/ 4.5
+        vis.arcInnerRadius = vis.radiusAreas*4/5
 
         vis.svg = d3.select("#" + vis.parentElement).append("svg")
             .attr("width", vis.width+vis.margin.left+vis.margin.right)
-            .attr("height", 2000)
+            .attr("height", vis.height)
+            .attr("class", "whiteboard")
             .append('g')
+            .attr('transform', `translate (${vis.width/2}, ${vis.height/2})`)
+            .style("position", "absolute")
+            .style("top", '0px')
+            .style("left", '0px');
+
+        vis.centerDiv = d3.select("svg")
+            .append('g')
+            // .attr("height", 500)
+            // .attr("width", 500)
+            .attr("class", "mainTitle")
             .attr('transform', `translate (${vis.width/2}, ${vis.height/2})`);
+
 
         vis.wrangleData();
 
     }
+
+
 
     wrangleData(){
         let vis = this;
@@ -68,7 +83,6 @@ class visRadial {
         });
 
 
-
         vis.filteredData.forEach(function(faculty, index){
             let title = faculty['Title'];
             let teachingAreas = faculty['Teaching Areas'].split('|');
@@ -87,14 +101,11 @@ class visRadial {
                     })
                 }
 
-
                 vis.Links.push({
                     "source": title,
                     "target": area,
                     "lvl": 0
                 })
-
-
             })
         });
 
@@ -120,9 +131,6 @@ class visRadial {
                 "lvl": 1
             })
         });
-
-        // vis.Links[0].source="Amir Yacoby"
-        // vis.Links[0].target = "Applied Physics"
 
         // z: For the donut diagram, roll the teaching data to get a list of faculty per each area
         vis.teachingAreasRolled=d3.rollups(vis.AreasDict, v=>v.length, d=>d.area)
@@ -172,13 +180,12 @@ class visRadial {
                 d.circleRadius = vis.radiusFaculty;
             } else { // level 0 = area/department
                 d.value = vis.areaDonutData[d.name]
-                d.nodeRadius = 3;
+                d.nodeRadius = 1;
                 d.circleRadius = vis.radiusAreas;
             }
 
         });
 
-        console.log(vis.Links)
 
         vis.Links.forEach(function (d,i) {
             d.source = vis.Nodes.filter(obj => {return obj.name === d.source})[0];
@@ -188,20 +195,20 @@ class visRadial {
 
         });
 
-        console.log(vis.Links)
-
         //
+        vis.setTitle(hover=false);
         vis.prepareNodes()
-        vis.prepareAreaDonuts()
+        vis.drawAreaDonut()
 
     }
 
     prepareNodes() {
         let vis = this;
 
+        /////// NODES
 
         vis.pie = d3.pie()
-            .value(function (d) {;
+            .value(function (d) {
                 return d[1].value;
             })
         vis.radialNodeData = vis.pie(Object.entries(vis.Nodes))
@@ -222,6 +229,8 @@ class visRadial {
             vis.areaDonutAngles[d.data[0]]={ "startAngle": d.startAngle, "endAngle": d.endAngle}
         })
 
+
+
         vis.facultyAngleScale = d3.scaleLinear()
             .range([0, 2 * Math.PI])
             .domain([0, vis.facultyCount]);
@@ -237,11 +246,11 @@ class visRadial {
                 // console.log(d, vis.areaDonutAngles[d.data[1].name])
                 d.theta = vis.areaDonutAngles[d.data[1].name].startAngle+(vis.areaDonutAngles[d.data[1].name].endAngle-vis.areaDonutAngles[d.data[1].name].startAngle)/2-Math.PI/2
                 d.centerAngle = d.startAngle + ((d.endAngle - d.startAngle) / 2)
-                d.coords = vis.toCartesian(vis.radiusAreas*1.05,d.theta)
+                d.coords = vis.toCartesian(vis.radiusAreas*1.02,d.theta)
             } else if (d.data[1].lvl === 1) {
                 d.theta = vis.facultyAngleScale(i)
                 d.centerAngle = d.startAngle + ((d.endAngle - d.startAngle) / 2)
-                d.coords = vis.toCartesian(vis.radiusFaculty*1.3,d.theta)
+                d.coords = vis.toCartesian(vis.radiusFaculty*1.2,d.theta)
             } else if (d.data[1].lvl ===2) {
                 d.theta = vis.centerAngleScale(i)
                 d.centerAngle = d.startAngle + ((d.endAngle - d.startAngle) / 2)
@@ -251,6 +260,16 @@ class visRadial {
             }
         })
 
+        vis.drawNodes()
+
+
+
+
+    }
+
+    drawNodes(){
+
+        let vis = this;
 
         vis.nodeGroups = vis.svg.selectAll('.nodeGroup')
             .data(vis.radialNodeData)
@@ -269,100 +288,107 @@ class visRadial {
             .attr("cy", d=>d.coords[1])
             .attr("fill","none")
             .attr("stroke", "black")
-            .attr("stroke-width", "2px")
+            .attr("stroke-width", function(d){
+                if(d.data[1].lvl === 0){ return "px"; }
+                else if(d.data[1].lvl === 2){ return "0px"; }
+                else{ return "0px"; }
+            })
             .attr("fill", function(d){
-                if(d.data[1].lvl === 0){ return vis.colorAreas(d.data[1].name); }
-                else if(d.data[1].lvl === 2){ return "#2eecec"; }
-                else{ return "#c60b0b"; }
+                if(d.data[1].lvl === 0){ return "black"; }
+                else if(d.data[1].lvl === 2){ return "#93a1ad"; }
+                else{ return "#A51C30"; }
             })
             .on("mouseover", function(event,d){
+                // vis.showModal(d)
+                vis.setTitle(true,d)
                 d3.select(this)
                     .attr('stroke-width', '2px')
-                    .attr('stroke', 'red')
-               })
+                    .attr('stroke', function(d){
+                        if (d.data[1].lvl === 2){ return "#A51C30"; }
+                        else if (d.data[1].lvl === 1) { return "black"; }
+                    })
+
+            })
+            .on("mouseout", function(event,d){
+                d3.select(this)
+                    .attr('stroke-width', '0px')
+            })
+            // .on("click", function(event,d){vis.showModal(d)})
 
         nodes.merge(nodesEnter)
             .attr("x", function (d) { return d.x; })
             .attr("y", function (d) { return d.y; })
 
-        vis.diagonal = function link(d) {
-            // console.log("diagonal",d)
-            return "M" + d.source.y + "," + d.source.x
-                + "C" + (d.source.y + d.target.y) / 2 + "," + d.source.x
-                + " " + (d.source.y + d.target.y) / 2 + "," + d.target.x
-                + " " + d.target.y + "," + d.target.x;
-        };
+        // vis.addCenterLabels()
 
-        let links = vis.svg.selectAll(".link").data(vis.Links, d=>d.id)
-        links.exit().remove();
 
-        let linksEnter = links.enter().append("path", "g")
-            .attr("class", "link")
-            .attr("id", li => li.id)
-            .attr("stroke", function(li){
-                // console.log(li)
-                if(li.target.lvl === 0){ return vis.colorAreas(li.target.name); }
-                else{ return vis.colorCenters(li.target.name); }
-            });
+        /////// LINKS
 
+        // vis.diagonal = function link(d) {
+        //     // console.log("diagonal",d)
+        //     return "M" + d.source.y + "," + d.source.x
+        //         + "C" + (d.source.y + d.target.y) / 2 + "," + d.source.x
+        //         + " " + (d.source.y + d.target.y) / 2 + "," + d.target.x
+        //         + " " + d.target.y + "," + d.target.x;
+        // };
+        //
+        // let links = vis.svg
+        //     .selectAll(".link")
+        //     .data(vis.Links, d=>d.id)
+        //
+        // links.exit().remove();
+        //
+        // let linksEnter = links.enter()
+        //     .append("path", "g")
+        //     .attr("class", "link")
+        //     .attr("id", li => li.id)
+        //     .attr("stroke", function(li){
+        //         // console.log(li)
+        //         if(li.target.lvl === 0){ return vis.colorAreas(li.target.name); }
+        //         else{ return vis.colorCenters(li.target.name); }
+        //     });
+        //
         // links.merge(linksEnter)
-        //     .attr("d", function (li) {
-        //         let oTarget;
-        //         let oSource;
+        //     .attr("d", function(link,i){
+        //         vis.calculateCurve(link,i)
         //
-        //         if(li.target.lvl === 0){
-        //             oTarget = {
-        //                 x: li.target.y + 0.5 * vis.boxHeightArea,
-        //                 y: li.target.x
-        //             };
-        //             oSource = {
-        //                 x: li.source.y + 0.5 * vis.boxHeight,
-        //                 y: li.source.x
-        //             };
         //
-        //             if (oSource.y < oTarget.y) {
-        //                 oSource.y += vis.boxWidth;
-        //             } else {
-        //                 oTarget.y += vis.boxWidthArea;
-        //             }
-        //         } else{
-        //             oTarget = {
-        //                 x: li.target.y + 0.5 * vis.boxHeightCenter,
-        //                 y: li.target.x
-        //             };
-        //             oSource = {
-        //                 x: li.source.y + 0.5 * vis.boxHeight,
-        //                 y: li.source.x
-        //             };
         //
-        //             if (oSource.y < oTarget.y) {
-        //                 oSource.y += vis.boxWidth;
-        //             } else {
-        //                 oTarget.y += vis.boxWidthCenter;
-        //             }
-        //         }
-        //
-        //         return vis.diagonal({
-        //             source: oSource,
-        //             target: oTarget
-        //         });
-            })
-
+        //     })
     }
 
-    prepareAreaDonuts(){
+    // addCenterLabels(){
+    //     let vis = this;
+    //
+    //     vis.labels = vis.svg
+    //         .selectAll('.centerCircleLabels')
+    //         .attr("class", "centerCircleLabels")
+    //         .attr("font-family", "sans-serif")
+    //         .attr("font-size", 12)
+    //         .attr("fill", "white")
+    //         .attr("stroke-width", 0.3)
+    //         .attr("stroke", "black")
+    //         .data(vis.radialNodeData)
+    //
+    //
+    //
+    // }
+
+    drawAreaDonut(){
         let vis = this;
 
 
-
-// Build the department donut chart: Basically, each part of the pie is a path that we build using the arc function.
-        vis.svg
+        vis.areaArcs =  vis.svg
             .selectAll('whatever')
             .data(vis.areaDonutDataReady)
+
+// Build the department donut chart: Basically, each part of the pie is a path that we build using the arc function.
+
+        vis.areaArcs
             .enter()
             .append('path')
             .attr('d', d3.arc()
-                .innerRadius(vis.radiusAreas * 4 / 5)         // This is the size of the donut hole
+                .innerRadius(vis.arcInnerRadius)         // This is the size of the donut hole
                 .outerRadius(vis.radiusAreas)
                 .cornerRadius(2)
                 .padAngle(0.03)
@@ -370,11 +396,113 @@ class visRadial {
             .attr('fill', function (d) {
                 return (vis.colorAreas(d.data[0]))
             })
-            // .attr("stroke", "black")
-            // .style("stroke-width", "2px")
+            .attr("stroke", "black")
+            .attr("stroke-width", "0px")
             .style("opacity",1)
+            .on("mouseover", function(event,d){
+                // vis.showModal(d)
+                d3.select(this)
+                    .attr('stroke-width', '2px')
+                    .attr('stroke', "black")
+            })
+            .on("mouseout", function(event,d){
+                d3.select(this)
+                    .attr('stroke-width', '0px')
+            })
             .append("svg:title")
             .text(d => d.data[0])
+
+
+        vis.areaArcs.exit().remove();
+
+    }
+
+    setTitle(hover,d){
+        let vis = this;
+
+
+
+        // vis.centerDiv
+        //     .append("text")
+        //     .text("SEAS FACULTY")
+        //     .attr("class", "main")
+        //     .attr("position", "top")
+
+
+
+        vis.centerDiv
+            .append("text")
+            .text("SEAS FACULTY")
+            .attr("color", "black")
+            .attr("font-size", "2em")
+            .attr("z-index", 100000)
+            .attr("dy", "-2em")
+            .attr("text-anchor", "middle")
+
+        vis.centerDiv
+            .append("text")
+            .text("are involved in")
+            .attr("color", "black")
+            .attr("font-size", "1.5em")
+            .attr("z-index", 100000)
+            .attr("dy", "-1em")
+            .attr("text-anchor", "middle")
+
+        vis.centerDiv.selectAll(".defaultText").remove()
+        vis.centerDiv.selectAll(".hoverText").remove()
+
+        if (hover){
+
+
+
+            console.log(d.data.name)
+
+            vis.centerDiv
+                .append("text")
+                .text(d.data[1].name)
+                .attr("color", "black")
+                .attr("font-size", "2em")
+                .attr("z-index", 100000)
+                .attr("dy", "1em")
+                .attr("text-anchor", "middle")
+                .attr("class", "hoverText")
+
+        } else {
+
+
+
+            vis.centerDiv
+                .append("text")
+                .text(vis.centerCount + " CENTERS  ")
+                .attr("color", "black")
+                .attr("font-size", "2em")
+                .attr("z-index", 100000)
+                .attr("dy", "1em")
+                .attr("text-anchor", "middle")
+                .attr("class", "defaultText")
+
+
+            vis.centerDiv
+                .append("text")
+                .text(" & ")
+                .attr("color", "black")
+                .attr("font-size", "1em")
+                .attr("z-index", 100000)
+                .attr("dy", "3.5em")
+                .attr("text-anchor", "middle")
+                .attr("class", "defaultText")
+
+            vis.centerDiv
+                .append("text")
+                .text(vis.areaCount + " DEPARTMENTS")
+                .attr("color", "black")
+                .attr("font-size", "2em")
+                .attr("z-index", 100000)
+                .attr("dy", "3em")
+                .attr("text-anchor", "middle")
+                .attr("class", "defaultText")
+        }
+
 
     }
 
@@ -384,4 +512,10 @@ class visRadial {
         // console.log(radial, theta, x, y)
         return [x,y];
     }
+
+    calculateCurve(link,i){
+        // console.log(link)
+    }
+
+
 }
